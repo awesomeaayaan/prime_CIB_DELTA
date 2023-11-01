3f723b167007a8c13d2ec884fc47571b318f509c
Aayaan Gautam <aayaan.gautam1999@gmail.com>
2023-11-01 09:51:07 +0545

    uploaded final_test ipynb file

--- final_manager (1).py
+++ final_manager (1).py
@@ -44,50 +44,50 @@ for data in datas["BulkOffLoading"]["Individuals"]["Item"]:
     print(f"--------------- Processed Data number {num} -----------------------" )
     print(dicts)
     num += 1
-    # if num == 51:
-    #     break
+    if num == 3:
+        break
 
-    # appending to df
-    df.loc[len(df)] = dicts
-# df = pd.DataFrame(columns=[
-#         'Name',
-#         'PANDetails', 
-#         'CompanyDetails',
-#         'BlackLists' 
-#     ])
+#     # appending to df
+#     df.loc[len(df)] = dicts
+# # df = pd.DataFrame(columns=[
+# #         'Name',
+# #         'PANDetails', 
+# #         'CompanyDetails',
+# #         'BlackLists' 
+# #     ])
 
-# num = 0
-for data in datas["BulkOffLoading"]["Institutions"]["Item"]:
-      # for key_value in data:
-    print("--------------------------- REal Data --------------------- \n")
-    print(data)
-    dicts = {}
-    data_keys = list(data.keys())
-    for data_key in data_keys:
-        if isinstance(data[data_key], str):
-            dicts[data_key] = data[data_key]
-        else:
-            # maintain Citizenship details
-            keys = list(data[data_key].keys())
-            data_count = int(data[data_key][keys[0]])
-            if data_count > 1:
-                details = data[data_key][keys[1]]
-                dicts[data_key] = ['|'.join(entry.values()) for entry in details]
-            elif data_count == 0:
-                dicts[data_key] = ""
-            else:
-                details = data[data_key][keys[1]]
-                dicts[data_key] = '|'.join(details.values())
+# # num = 0
+# for data in datas["BulkOffLoading"]["Institutions"]["Item"]:
+#       # for key_value in data:
+#     print("--------------------------- REal Data --------------------- \n")
+#     print(data)
+#     dicts = {}
+#     data_keys = list(data.keys())
+#     for data_key in data_keys:
+#         if isinstance(data[data_key], str):
+#             dicts[data_key] = data[data_key]
+#         else:
+#             # maintain Citizenship details
+#             keys = list(data[data_key].keys())
+#             data_count = int(data[data_key][keys[0]])
+#             if data_count > 1:
+#                 details = data[data_key][keys[1]]
+#                 dicts[data_key] = ['|'.join(entry.values()) for entry in details]
+#             elif data_count == 0:
+#                 dicts[data_key] = ""
+#             else:
+#                 details = data[data_key][keys[1]]
+#                 dicts[data_key] = '|'.join(details.values())
 
-    print(f"--------------- Processed Data number {num} -----------------------" )
-    print(dicts)
-    num += 1
-    # if num == 11:
-    #     break
+#     print(f"--------------- Processed Data number {num} -----------------------" )
+#     print(dicts)
+#     num += 1
+#     # if num == 11:
+#     #     break
 
-    # appending to df
-    df.loc[len(df)] = dicts
+#     # appending to df
+#     df.loc[len(df)] = dicts
 
 
-df.to_excel("black_list.xlsx", index=False)
+# df.to_excel("black_list_new.xlsx", index=False)
     
