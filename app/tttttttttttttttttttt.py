271c3d351c48ceb509605369e6d8d4cc92c470f1
Aayaan Gautam <58321874+awesomeaayaan@users.noreply.github.com>
2023-12-04 11:46:52 +0545

    Update tttttttttttttttttttt.py

--- app/tttttttttttttttttttt.py
+++ app/tttttttttttttttttttt.py
@@ -14,3 +14,25 @@ if first_run_flag:
 else:
     print("Performing subsequent run actions...")
     print("orange")
+
+
+from datetime import datetime, timedelta
+con = sqlite3.connect(Constants.DB_NAME)
+cur = con.cursor()
+today_date = datetime.now().date()
+query = (f'''
+    SELECT * FROM {Constants.REPORT}
+    WHERE DATE(created_date) = ?;
+    ''',(today_date,))
+# logger = self.run_item.logger
+con.row_factory = sqlite3.Row
+# logger.info(f'Reading data having status pending from table  {Constants.REPORT}')
+data = con.execute(query).fetchall()
+if data:
+    # logger.info(f'All database data retrivied.')
+    temp_value = [{str(key): item[key] for key in item.keys()} for item in data]
+    # display(f'Pending data is {temp_value}')
+    # temp_value.to_excel('test_data_cib.xlsx',index=False)
+    print(temp_value) 
+else:
+    print("didn't get data from data base of today date")
